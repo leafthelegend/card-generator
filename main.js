6ee216eb1fe5bdb7c3717bb1dd07b94302d09d3f
leafthelegend <liefcode@gmail.com>
2024-01-30 17:57:53 +1000

    printing tweaks

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var previewButton = document.getElementById('preview-button');
 var generateButton = document.getElementById('generate-button');
+const N = 9;
 
 previewButton.addEventListener('click',preview)
 
@@ -16,16 +17,32 @@ function generate(){
     let data = getData();
     let pageBox = document.createElement('div');
     pageBox.classList.add('page-box');
-    data.forEach((str)=>{
-        str = str.trim();
-        card = document.getElementById('preview').cloneNode(true);
-        card.querySelector('.card-text').textContent = str;
-        pageBox.appendChild(card);
-    })
-    printWindow = window.open()
-    printWindow.document.head.innerHTML = window.document.head.innerHTML;
+    let background = document.createElement('div');
+    background.classList.add('background');
+    background.classList.add('page-box');
     let printContainer = document.createElement('div');
     printContainer.classList.add('print-container');
-    printContainer.appendChild(pageBox);
+    let i = 0;
+    data.forEach((str)=>{
+        i += 1;
+        i %= 9;
+        str = str.trim();
+        card = document.getElementById('preview').cloneNode(true);
+        card.classList.add('card');
+        card.querySelector('.card-text').textContent = str;
+        pageBox.appendChild(card);
+        if(i==0){
+            printContainer.appendChild(pageBox.cloneNode(true));
+            printContainer.appendChild(background.cloneNode(true));
+            pageBox.innerHTML = '';
+        }
+    });
+    if(i>0){
+        printContainer.appendChild(pageBox.cloneNode(true));
+        printContainer.appendChild(background.cloneNode(true));
+    }
+    printWindow = window.open()
+    printWindow.document.head.innerHTML = window.document.head.innerHTML;
     printWindow.document.body.appendChild(printContainer);
+    printWindow.print();
 }
