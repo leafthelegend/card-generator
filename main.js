132308d5ac1a9241147c16d1d07828f09466d82e
leafthelegend <liefcode@gmail.com>
2024-02-02 11:38:06 +1000

    New Backgrounds, Automatic resizing

--- main.js
+++ main.js
@@ -2,19 +2,40 @@ var previewButton = document.getElementById('preview-button');
 var generateButton = document.getElementById('generate-button');
 const N = 9;
 
-previewButton.addEventListener('click',preview)
+previewButton.addEventListener('click',()=>getData(preview))
 
-generateButton.addEventListener('click', generate);
+generateButton.addEventListener('click', ()=>getData(generate));
 
-function getData(){return document.getElementById('text-input').value.split("\n");}
-
-function preview(){
-    let data = getData();
-    let previewDiv = document.querySelector('.card-text');
-    previewDiv.textContent = data[0];
+function getData(callback){
+    if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
+        console.log('The File APIs are not fully supported in this browser.');
+        return;
+      }
+      let formFile = document.getElementById('file-input');
+      if (!formFile.files) {
+        console.log("This browser doesn't seem to support the `files` property of file inputs.");
+      } else if (!formFile.files[0]) {
+        let lines = document.getElementById('text-input').value.split("\n");
+        callback(lines);
+      } else {
+        let file = formFile.files[0];
+        let fr = new FileReader();
+        fr.onload = receivedText;
+        fr.readAsText(file);
+        function receivedText() {
+            let data = fr.result;
+            let lines = data.split('\n');
+            callback(lines);
+        }
+      }
 }
-function generate(){
-    let data = getData();
+
+function preview(data){
+    let previewDiv = document.querySelector('.inner-text');
+    previewDiv.textContent = data[0];
+    resize2fit(document.querySelector('.card-text'));
+}
+function generate(data){
     let pageBox = document.createElement('div');
     pageBox.classList.add('page-box');
     let background = document.createElement('div');
@@ -29,7 +50,7 @@ function generate(){
         str = str.trim();
         card = document.getElementById('preview').cloneNode(true);
         card.classList.add('card');
-        card.querySelector('.card-text').textContent = str;
+        card.querySelector('.inner-text').textContent = str;
         pageBox.appendChild(card);
         if(i==0){
             printContainer.appendChild(pageBox.cloneNode(true));
@@ -44,5 +65,19 @@ function generate(){
     printWindow = window.open()
     printWindow.document.head.innerHTML = window.document.head.innerHTML;
     printWindow.document.body.appendChild(printContainer);
-    printWindow.print();
+    printWindow.setTimeout(()=>{
+        for(el of printWindow.document.querySelectorAll('.card-text')){
+            console.log(el);
+            resize2fit(el);
+        }
+    },100);
+    printWindow.setTimeout(printWindow.print,300);
 }
+// function fitText()
+function resize2fit(el) {
+    if (!el.parentElement) return;
+    el.style.setProperty("font-size", "1em");
+    const {width: max_width, height: max_height} = el.getBoundingClientRect();
+    const {width, height} = el.children[0].getBoundingClientRect();
+    el.style.setProperty("font-size", Math.min(max_width/width, max_height/height)+"em");
+  }
